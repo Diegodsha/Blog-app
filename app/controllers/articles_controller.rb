56bbb66398c151b4cb381b4630c68ca19c0ef860
Rashad-Muntar <muntartoure@gmail.com>
2021-03-19 19:59:09 +0000

    add delete action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,6 +20,27 @@ class ArticlesController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    
+    redirect_to root_path
+  end
   
   private
   def article_params
