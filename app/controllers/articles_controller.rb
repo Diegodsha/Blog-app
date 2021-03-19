9e4063af43e1caa5026f1d61e3ea94572f296e32
Diego <diegodsha@hotmail.com>
2021-03-19 14:48:05 -0600

    Add comments controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     
-    if @article.after_save 
+    if @article.save 
       redirect_to @article
     else
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'articles#index'
 
-  resources :articles
+  resources :articles do
+    resources :comments
+
+  end
   # get 'articles', to: 'articles#index'
   # get 'articles/:id', to: 'articles#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
