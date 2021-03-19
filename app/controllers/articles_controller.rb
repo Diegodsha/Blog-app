7f473d4b6b4f1eed031796ec14c616bfb3780078
Diego <diegodsha@hotmail.com>
2021-03-19 16:47:27 -0600

    Fix rubocop offenses

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,15 +6,15 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
-  
+
   def new
     @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
-    
-    if @article.save 
+
+    if @article.save
       redirect_to @article
     else
       render :new
@@ -38,14 +38,13 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    
+
     redirect_to root_path
   end
-  
+
   private
+
   def article_params
     params.require(:article).permit(:title, :body)
   end
-  
-  
 end
